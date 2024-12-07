7fe494852d939abbf2f4ae6cc19b9083d3f4a0cd
withol <witekholdys@gmail.com>
2024-12-07 17:27:18 +0100

    Added a hashmap used to assign type names with type numbers

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ use crate::trips::circles;
 use crate::trips::center;
 
 use rand::Rng;
+use lazy_static::lazy_static;
 use std::f32::consts::PI;
 use std::collections::HashMap;
+use std::process::exit;
 
 mod shared;
 mod trips;
@@ -20,7 +22,7 @@ fn main()
     let args: Vec<String> = std::env::args().collect(); // Collecting the arguments
     let mut duration: f32 = 10.0; // The duration of the drug trip
     let mut no_stop: bool = false; // Whether or not to allow stopping the program with ctrl+c
-    let mut trip_type: i8 = rand::thread_rng().gen_range(0..2);
+    let mut trip_type: i16 = rand::thread_rng().gen_range(0..2);
 
     // Processing the arguments, and adjusting previously defined variables according to the flags
     process_args(&args, &mut duration, &mut no_stop, &mut trip_type);
@@ -47,10 +49,9 @@ fn main()
     ncurses::endwin();
 }
 
-
 // This function processes the arguments and adjusts the varables, according to them.
 // It can also stop the program on a wrong argument, or when a flag like -h has been provided 
-fn process_args(args: &Vec<String>, duration: &mut f32, no_stop: &mut bool, trip_type: &mut i8)
+fn process_args(args: &Vec<String>, duration: &mut f32, no_stop: &mut bool, trip_type: &mut i16)
 {
     let mut i: usize = 1;
     while i < args.len()
@@ -58,7 +59,7 @@ fn process_args(args: &Vec<String>, duration: &mut f32, no_stop: &mut bool, trip
         match args[i].as_str()
         {
             "--help" | "-h" => { 
-                print!("{}", HELP()); std::process::exit(0); 
+                println!("{}", *HELP); std::process::exit(0); 
             }
 
             "--list" | "-l" => {
@@ -76,14 +77,9 @@ fn process_args(args: &Vec<String>, duration: &mut f32, no_stop: &mut bool, trip
                     println!("no duration provided");
                     std::process::exit(1);
                 }
-                *duration = match args[i+1].parse()
-                {
-                    Ok(T) => {T} 
-                    Err(_) => {
-                        println!("wrong duration: {}", args[i+1]);
-                        std::process::exit(1);
-                    }
-                };
+
+                *duration = args[i+1].parse().expect("wrong duration");
+
                 i += 1;
             }
 
@@ -94,22 +90,9 @@ fn process_args(args: &Vec<String>, duration: &mut f32, no_stop: &mut bool, trip
                     std::process::exit(1);
                 }
 
-                *trip_type = match match args[i+1].parse::<String>()
-                { 
-                    Ok(T) => T,
-                    Err(_) => {
-                        println!("wrong drug trip type: {}", args[i+1]);
-                        std::process::exit(1);
-                    }
-                }.as_str()
-                {
-                    "circles" => 0,
-                    "center" => 1,
-                    _ => {
-                        println!("wrong drug trip type: {}", args[i+1]);
-                        std::process::exit(1);
-                    }
-                };
+                *trip_type = *DrugTrips.get( &args[i+1].parse::<String>().expect("wrong drug trip type"))
+                    .expect("wrong drug trip type");
+
                 i += 1;
             }
 
@@ -120,14 +103,7 @@ fn process_args(args: &Vec<String>, duration: &mut f32, no_stop: &mut bool, trip
                     std::process::exit(1);
                 }
 
-                *trip_type = match args[i+1].parse()
-                {
-                    Ok(T) => T,
-                    Err(_) => {
-                        println!("wrong drig trip type: {}", args[i+1]);
-                        std::process::exit(1);
-                    }
-                };
+                *trip_type = args[i+1].parse().expect("wrong drug trip number");
 
                 i += 1;
             }
@@ -141,21 +117,26 @@ fn process_args(args: &Vec<String>, duration: &mut f32, no_stop: &mut bool, trip
     }
 }
 
+// This variable holds the message displayed when -h flag is given  
+lazy_static!{
+    static ref HELP: String = String::from("
+        Simulate a drug trip whenever you mistype ls for lsd
+        Usage: lsd/ls-trip <flag> [...]
 
-// The only purpose of this function is to hold the help message, 
-// cuz I didn't feel like learning lifetime specifiers for static variables
-fn HELP() -> String
-{
-    String::from("
-    Simulate a drug trip whenever you mistype ls for lsd
-    Usage: lsd/ls-trip <flag> [...]
+        Avilable flags:
+            -h, --help              print this menu
+            -l, --list              list drug trip types
+            -t, --type              drug trip type name
+            -T, --type-number       drug trip type number
+            -d, --duration          duration of the drug trip in seconds
+            -c, --no-cancel         disable the user's ability to stop the program with ctrl-c
+        ");
+}
 
-    Avilable flags:
-        -h, --help              print this menu
-        -l, --list              list drug trip types
-        -t, --type              drug trip type name
-        -T, --type-number       drug trip type number
-        -d, --duration          duration of the drug trip in seconds
-        -c, --no-cancel         disable the user's ability to stop the program with ctrl-c
-    ")
+// This is a hashmap, used to assign different trip type names with the trip numbers
+lazy_static!{
+    static ref DrugTrips: HashMap<String, i16> = HashMap::<String, i16>::from([
+        (String::from("circles"), 0),
+        (String::from("center"), 1),
+    ]);
 }
